3a5668dff41f14bf400123e82b2791226a9206ae
Pranav Pise <itspranavpise@gmail.com>
2025-02-25 13:15:22 +0530

    fix: camelcasing issue fixed

--- src/repos/user-repo.js
+++ src/repos/user-repo.js
@@ -1,14 +1,15 @@
 const pool = require("../pool");
+const toCamelCase = require("../repos/utils/to-camel-case");
 
 class UserRepo {
   static async find() {
     const { rows } = await pool.query("SELECT * FROM users;");
-    return rows;
+    return toCamelCase(rows);
   }
 
   static async findById(id) {
     const { rows } = await pool.query(`SELECT * FROM users WHERE id = ${id}`);
-    return rows[0];
+    return toCamelCase(rows)[0];
   }
 
   static async insert() {}
