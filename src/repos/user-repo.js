7e77c64a1d0eaf7c4d74ae6e5a677e2e0b8fc994
Pranav Pise <itspranavpise@gmail.com>
2025-02-25 16:09:48 +0530

    add: crud functionalities, ps: something's wrong with error handling

--- src/repos/user-repo.js
+++ src/repos/user-repo.js
@@ -8,15 +8,37 @@ class UserRepo {
   }
 
   static async findById(id) {
-    const { rows } = await pool.query(`SELECT * FROM users WHERE id = ${id}`);
+    const { rows } = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      id,
+    ]);
     return toCamelCase(rows)[0];
   }
 
-  static async insert() {}
+  static async insert(username, bio) {
+    return await pool.query(
+      "INSERT INTO users(username, bio) VALUES ($1, $2);",
+      [username, bio]
+    );
+  }
 
-  static async update() {}
+  static async update(id, username, bio) {
+    const user = pool.query("SELECT * FROM users WHERE id = $1;", [id]);
+    if (!user) {
+      return new Error("User not found");
+    }
+    return await pool.query(
+      "UPDATE users SET username = $1, bio = $2 WHERE id = $3;",
+      [username, bio, id]
+    );
+  }
 
-  static async delete() {}
+  static async delete(id) {
+    const user = pool.query("SELECT * FROM users WHERE id = $1;", [id]);
+    if (!user) {
+      return new Error("User not found");
+    }
+    return await pool.query("DELETE FROM users WHERE id = $1;", [id]);
+  }
 }
 
 module.exports = UserRepo;
--- src/routes/users.js
+++ src/routes/users.js
@@ -17,10 +17,33 @@ router.get("/users/:id", async (req, res) => {
   return res.send(user);
 });
 
-router.post("/users", async (req, res) => {});
+router.post("/users", async (req, res) => {
+  const { username, bio } = req.body;
+  const response = await UserRepo.insert(username, bio);
 
-router.put("/users/:id", async (req, res) => {});
+  res.send("User inserted successfully");
+});
 
-router.delete("/users/:id", async (req, res) => {});
+router.put("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { username, bio } = req.body;
+
+  try {
+    await UserRepo.update(id, username, bio);
+    res.send("User updated successfully");
+  } catch (err) {
+    return res.sendStatus(404);
+  }
+});
+
+router.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const response = await UserRepo.delete(id);
+
+  if (!response) {
+    return res.sendStatus(404);
+  }
+  res.send("User deleted successfully");
+});
 
 module.exports = router;
