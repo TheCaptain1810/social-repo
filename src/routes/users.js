9fcbf267dd7d45719fbc34e531f0063603660c68
Pranav Pise <itspranavpise@gmail.com>
2025-02-25 12:57:43 +0530

    add: get requests, ps: solve security issue

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const UserRepo = require("../repos/user-repo");
 
 const router = express.Router();
 
-router.get("/users", async (req, res) => {});
+router.get("/users", async (req, res) => {
+  const users = await UserRepo.find();
+  res.send(users);
+});
 
-router.get("/users/:id", async (req, res) => {});
+router.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await UserRepo.findById(id);
+  if (!user) {
+    return res.sendStatus(404);
+  }
+  return res.send(user);
+});
 
 router.post("/users", async (req, res) => {});
 
